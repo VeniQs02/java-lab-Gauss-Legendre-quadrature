6bcb250c73a8d235d9517b9d316293dda17d3a76
Jakub Kańgowski <jakub.kangowski7@vp.pl>
2023-05-01 17:30:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        GaussLegendreQuadrature gausik = new GaussLegendreQuadrature(4);
+        gausik.weightAbscissaPrinter();
+    }
+}
